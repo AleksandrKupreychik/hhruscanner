a87c9c39f806b4e4f90ad64a06ca050b46d49517
a.kupreychik <kupreychik1994@gmail.com>
2025-04-26 18:36:56 +0700

    add kafka messaging

---
+++ src/main/java/com/example/hhruscanner/kafka/KafkaMessage.java
@@ -0,0 +1,10 @@
+package com.example.hhruscanner.kafka;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class KafkaMessage {
+    private UUID id;
+}
---
+++ src/main/java/com/example/hhruscanner/kafka/KafkaMessageProducer.java
@@ -0,0 +1,25 @@
+package com.example.hhruscanner.kafka;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Slf4j
+@Service
+public class KafkaMessageProducer {
+    @Value("spring.kafka.topic.newVacancies")
+    private String NEW_VACANCIES_TOPIC;
+    private final KafkaTemplate<String, KafkaMessage> kafkaTemplate;
+
+    public KafkaMessageProducer(KafkaTemplate<String, KafkaMessage> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendNewVacancyMessage(VacancyMessage message) {
+        kafkaTemplate.send(NEW_VACANCIES_TOPIC, message);
+    }
+}
+
